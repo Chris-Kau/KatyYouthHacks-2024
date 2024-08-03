4229530ee7a603e93a1580a30bcb134f26e714fc
Chris-Kau <uhchris04@gmail.com>
2024-08-02 19:47:41 -0700

    added sections

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import tkinter
 import customtkinter
-#Chris
-#########################################################################
+
 mainwindow = customtkinter.CTk()
 mainwindow._set_appearance_mode("System")
+mainwindow.geometry("600x400")
+
+ai_chat_section = customtkinter.CTkFrame(master = mainwindow, width = 500, fg_color="white")
+ai_chat_section.pack(side = "right", fill = "y")
+
+event_info_section = customtkinter.CTkFrame(master = mainwindow, width = 300, fg_color="blue")
+event_info_section.pack(side="left", fill='y')
+
+calendar_section = customtkinter.CTkFrame(master = mainwindow, fg_color= "green")
+calendar_section.pack(fill = "both", expand=True)
+
+#Chris
+#########################################################################
 
 
 
