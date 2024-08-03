de0e030f7e05eb8becdb0d86ed64198bcb45f443
Chris-Kau <uhchris04@gmail.com>
2024-08-02 21:48:00 -0700

    added test day class

--- Calendar/calendar_section.py
+++ Calendar/calendar_section.py
@@ -1,6 +1,9 @@
 import tkinter
 import customtkinter
+from Calendar.Day import Day
 class CalendarSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.pack(side = "right", fill = "y")
+        self.monday = Day(self, "Monday", width = 50, height = 50)
+        self.pack(fill = "both", expand = True)
+        self.monday.pack(padx = 10, pady = 10)
