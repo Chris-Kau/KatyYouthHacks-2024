243620e2c67beec3a501789155cf54b220e219aa
Chris-Kau <uhchris04@gmail.com>
2024-08-02 21:48:38 -0700

    fixed pack option

--- EventInfo/event_info_section.py
+++ EventInfo/event_info_section.py
@@ -3,4 +3,4 @@ import customtkinter
 class EventInfoSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.pack(side = "left", fill = "y")
+        self.pack(side = "left", fill = "both")
