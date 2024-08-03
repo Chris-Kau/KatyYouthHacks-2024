e4a1fcd0bdcfc2e235f4aa8d3e3ed0fc0f26071b
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2024-08-03 09:43:08 -0700

    abstract the days

--- Calendar/Day.py
+++ Calendar/Day.py
@@ -2,10 +2,13 @@ import tkinter
 import customtkinter
 from Calendar.Event import Event
 class Day(customtkinter.CTkFrame):
-    def __init__(self, master, day, **kwargs):
+    def __init__(self, master, title, time, desc, **kwargs):
         super().__init__(master, **kwargs)
-        self.day = day
-        self.monday = Event(self, "00:00", "Homework", "PLEASE TELL ME TO DO MY HOMEWORK")
-        self.monday.pack()
+
+        # self.day = day
+        self.day = Event(self, title, time, desc)
+
+        # self.monday = Event(self, "Homework", "00:00", "PLEASE TELL ME TO DO MY HOMEWORK")
+        self.day.pack()
         
 
--- Calendar/calendar_section.py
+++ Calendar/calendar_section.py
@@ -4,6 +4,11 @@ from Calendar.Day import Day
 class CalendarSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.monday = Day(self, "Monday", width = 50, height = 50)
+
+        self.sunday = Day(self, "Homework", "00:00", "DO YOUR HOMEWORK", width = 50, height = 50)
+        self.sunday.pack(padx = 10, pady = 10)
+
+
+        # self.monday = Day(self, "Monday", width = 50, height = 50)
         self.pack(fill = "both", expand = True)
-        self.monday.pack(padx = 10, pady = 10)
+        # self.monday.pack(padx = 10, pady = 10)
