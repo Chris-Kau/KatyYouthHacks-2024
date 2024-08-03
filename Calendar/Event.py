013144f3ae10d9de4b99f909ef7876f7ecc2b5a6
Chris-Kau <uhchris04@gmail.com>
2024-08-02 21:48:24 -0700

    created event class

---
+++ Calendar/Event.py
@@ -0,0 +1,13 @@
+import tkinter
+import customtkinter
+class Event(customtkinter.CTkTextbox):
+    def __init__(self, master, time, title, description, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(text_color = "black", fg_color = "white", wrap = "word") #read-only
+        self.tag_config("center", justify = "center")
+        self.insert("1.0", title + " ") #insert at line 0, character 0
+        self.insert("2.0", time + "\n") 
+        self.insert("3.0", description + "\n")
+        self.pack()
+
+
