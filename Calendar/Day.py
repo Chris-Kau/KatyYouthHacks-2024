d344a339742559af32f90e5d9e50d36729edc963
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2024-08-03 11:23:13 -0700

    YEAHHH DAYS OF THE WEEK GRID

--- Calendar/Day.py
+++ Calendar/Day.py
@@ -2,13 +2,28 @@ import tkinter
 import customtkinter
 from Calendar.Event import Event
 class Day(customtkinter.CTkFrame):
-    def __init__(self, master, title, time, desc, **kwargs):
+    def __init__(self, master, day, col, **kwargs): # title, time, desc, 
         super().__init__(master, **kwargs)
 
-        # self.day = day
-        self.day = Event(self, title, time, desc)
+        self.update()
+        wid = (self.winfo_screenwidth()-550)/7 # 550 is width of other two sections combined
+        hei = self.winfo_screenheight()
 
+        self.label = customtkinter.CTkLabel(self)
+        self.label.configure(text = day)
+        self.label.configure(width = wid-19, height = hei-600)
+
+        self.label.grid(row=0, column=0, padx=10, pady = 300) 
+        # self.grid_columnconfigure(col, weight=col)
+        
+
+        self.pack()
+
+
+        # self.day = Event(self, title, time, desc)
+        # self.day.pack()
+        
         # self.monday = Event(self, "Homework", "00:00", "PLEASE TELL ME TO DO MY HOMEWORK")
-        self.day.pack()
+        
         
 
--- Calendar/calendar_section.py
+++ Calendar/calendar_section.py
@@ -5,10 +5,32 @@ class CalendarSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.sunday = Day(self, "Homework", "00:00", "DO YOUR HOMEWORK", width = 50, height = 50)
-        self.sunday.pack(padx = 10, pady = 10)
+
+        self.sunday = Day(self, "sunday", 0)
+        self.sunday.pack(side = "left")
+
+        self.monday = Day(self, "monday", 1)
+        self.monday.pack(side = "left")
+
+        self.tuesday = Day(self, "tuesday", 2)
+        self.tuesday.pack(side = "left")
+
+        self.wednesday = Day(self, "wednesday", 3)
+        self.wednesday.pack(side = "left")
+
+        self.thursday = Day(self, "thursday", 4)
+        self.thursday.pack(side = "left")
+
+        self.friday = Day(self, "friday", 5)
+        self.friday.pack(side = "left")
+
+        self.saturday = Day(self, "saturday", 6)
+        self.saturday.pack(side = "left")
 
 
-        # self.monday = Day(self, "Monday", width = 50, height = 50)
+        # self.sunday = Day(self, "Homework", "00:00", "DO YOUR HOMEWORK", width = 50, height = 50)
+        # self.sunday.pack(padx = 10, pady = 10)
+
+
         self.pack(fill = "both", expand = True)
-        # self.monday.pack(padx = 10, pady = 10)
+
