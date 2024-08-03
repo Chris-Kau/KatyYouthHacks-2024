e31fc80b8d7fc43fa8a8fdec962c69c8db1732f4
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-08-03 16:29:46 -0700

    updated variable names and added conversation

--- AiChat/ai_chat_section.py
+++ AiChat/ai_chat_section.py
@@ -2,9 +2,14 @@ import tkinter
 import customtkinter
 from AiChat.gpt2 import GPT
 from AiChat.usertextbox import UserTextBox
+from AiChat.conversation import Conversation
 class AiChatSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
+        self.pack_propagate(False)
         self.pack(side = "right", fill = "both", expand = True)
-        self.bottom_frame = UserTextBox(self, width = 250, height = 100, corner_radius = 10, border_color = "black", fg_color = "transparent", border_width = 2)
-        self.bottom_frame.pack(side = "bottom", pady = 10)
+        self.usertextbox = UserTextBox(self, width = 250, height = 100, corner_radius = 10, border_color = "black", fg_color = "transparent", border_width = 2)
+        self.usertextbox.pack(side = "bottom", pady = 10)
+
+        self.conversation = Conversation(self, fg_color = "#bfbfbf")
+        self.conversation.pack(side = "top", padx = 10, pady = 10, fill = "both", expand = True)
