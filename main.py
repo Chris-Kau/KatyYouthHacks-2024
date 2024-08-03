e6724e3f90f555a52e9d9dda9649d0b415a7ba20
mintcat3 <19940947+mintcat3@users.noreply.github.com>
2024-08-03 09:27:15 -0700

    widths

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ class App(customtkinter.CTk):
         self._set_appearance_mode("System")
         self.geometry("1200x800")
 
-        self.ai_chat_section = AiChatSection(master = self, width = 500, fg_color = "white")
+        self.ai_chat_section = AiChatSection(master = self, width = 300, fg_color = "white")
         self.ai_chat_section.pack(side = "right", fill = "both")
 
-        self.event_info_section = EventInfoSection(master = self, width = 300, fg_color="blue")
+        self.event_info_section = EventInfoSection(master = self, width = 250, fg_color="blue")
         self.event_info_section.pack(side = "left", fill = "both")
 
         self.calendar_section = CalendarSection(master = self, fg_color= "green")
