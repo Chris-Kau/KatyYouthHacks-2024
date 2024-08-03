fa948134d389317113eac34de48f7eb2717a9bd5
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-08-03 14:37:32 -0700

    added texxt box

--- AiChat/ai_chat_section.py
+++ AiChat/ai_chat_section.py
@@ -1,7 +1,10 @@
 import tkinter
 import customtkinter
+from AiChat.gpt2 import GPT
+from AiChat.usermessage import UserMessage
 class AiChatSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.pack(fill = "both", side = "right")
-        
+        self.pack(side = "right", fill = "both")
+        self.bottom_frame = UserMessage(self, width = 250, height = 50, fg_color = "grey")
+        #bottom_frame.pack(side = "bottom")
