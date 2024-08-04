35d9a98f1b88a92aa43e2c499eba557c0666a573
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-08-03 23:28:03 -0700

    comment out pack

--- AiChat/ai_chat_section.py
+++ AiChat/ai_chat_section.py
@@ -9,7 +9,7 @@ class AiChatSection(customtkinter.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.pack_propagate(False)
-        self.pack(side = "right", fill = "both", expand = True)
+        #self.pack(side = "right", fill = "both", expand = True)
         self.gpt = GPT()
         self.usertextbox = customtkinter.CTkTextbox(self, width = 250, height = 100, corner_radius = 10, border_color = "black", fg_color = "transparent", border_width = 2)
         self.usertextbox.bind("<Return>", self.on_enter_pressed)
